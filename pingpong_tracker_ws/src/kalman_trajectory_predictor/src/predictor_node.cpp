c0580822e9d800445b80f54230f436dbfc2610f5
heyeuu <2829004293@qq.com>
2025-08-20 01:44:12 +0800

    update .clang-format

--- pingpong_tracker_ws/src/kalman_trajectory_predictor/src/predictor_node.cpp
+++ pingpong_tracker_ws/src/kalman_trajectory_predictor/src/predictor_node.cpp
@@ -1,7 +1,11 @@
-// #include <rclcpp/rclcpp.hpp>
+#include "cv_bridge/cv_bridge.h"
+#include "geometry_msgs/msg/point.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
-int main(int argc, char * argv[])
-{
+#include "opencv2/opencv.hpp"
+
+int main(int argc, char *argv[]) {
   // 初始化 ROS 2
   //   rclcpp::init(argc, argv);
   // 创建并运行你的节点
