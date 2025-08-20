73dc9416f5db9b7b4c8e871593c3fb7d7940bfb8
heyeuu <2829004293@qq.com>
2025-08-20 23:46:19 +0800

    feat(prediction): add trajectory prediction node

--- pingpong_tracker_ws/src/kalman_trajectory_predictor/include/kalman_trajectory_predictor/kalman_filter.hpp
+++ pingpong_tracker_ws/src/kalman_trajectory_predictor/include/kalman_trajectory_predictor/kalman_filter.hpp
@@ -2,11 +2,9 @@
 
 #include "opencv2/opencv.hpp"
 
-class KalmanFilter
-{
+class KalmanFilter {
 public:
-  KalmanFilter()
-  {
+  KalmanFilter() {
     int state_dim = 6;
 
     int measurement_dim = 3;
@@ -16,40 +14,36 @@ public:
     kf_.init(state_dim, measurement_dim, control_dim);
 
     kf_.transitionMatrix =
-      (cv::Mat_<double>(6, 6) << 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1,
-       0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1);
+        (cv::Mat_<float>(6, 6) << 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1,
+         0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1);
 
-    kf_.measurementMatrix =
-      (cv::Mat_<double>(3, 6) << 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+    kf_.measurementMatrix = (cv::Mat_<float>(3, 6) << 1, 0, 0, 0, 0, 0, 0, 1, 0,
+                             0, 0, 0, 0, 0, 1, 0, 0, 0);
 
     cv::setIdentity(kf_.processNoiseCov, cv::Scalar::all(1e-2));
 
     cv::setIdentity(kf_.measurementNoiseCov, cv::Scalar::all(1e-1));
 
-    kf_.statePost = (cv::Mat_<double>(6, 1) << 0, 0, 0, 0, 0, 0, 0);
+    kf_.statePost = (cv::Mat_<float>(6, 1) << 0, 0, 0, 0, 0, 0, 0);
   };
 
-  cv::Point3f predict()
-  {
+  cv::Point3f predict() {
     cv::Mat prediction = kf_.predict();
 
-    return cv::Point3d(
-      prediction.at<double>(0, 0), prediction.at<double>(1, 0), prediction.at<double>(2, 0));
+    return cv::Point3f(prediction.at<float>(0, 0), prediction.at<float>(1, 0),
+                       prediction.at<float>(2, 0));
   }
 
-  void update(const cv::Point3d & measurement)
-  {
+  void update(const cv::Point3f &measurement) {
     cv::Mat measurement_mat =
-      (cv::Mat_<double>(3, 1) << measurement.x, measurement.y, measurement.z);
+        (cv::Mat_<float>(3, 1) << measurement.x, measurement.y, measurement.z);
 
     kf_.correct(measurement_mat);
   }
 
-  cv::Point3d getFilteredPosition() const
-  {
-    return cv::Point3d(
-      kf_.statePost.at<double>(0, 0), kf_.statePost.at<double>(1, 0),
-      kf_.statePost.at<double>(2, 0));
+  cv::Point3f getFilteredPosition() const {
+    return {kf_.statePost.at<float>(0, 0), kf_.statePost.at<float>(1, 0),
+            kf_.statePost.at<float>(2, 0)};
   }
 
 private:
--- pingpong_tracker_ws/src/kalman_trajectory_predictor/src/predictor_node.cpp
+++ pingpong_tracker_ws/src/kalman_trajectory_predictor/src/predictor_node.cpp
@@ -1,16 +1,12 @@
-#include "cv_bridge/cv_bridge.h"
-#include "geometry_msgs/msg/point.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
 
-#include "opencv2/opencv.hpp"
+#include "kalman_trajectory_predictor/kalman_filter.hpp"
+#include "kalman_trajectory_predictor/predictor_node.hpp"
 
+class PredictorNode;
 int main(int argc, char *argv[]) {
-  // 初始化 ROS 2
-  //   rclcpp::init(argc, argv);
-  // 创建并运行你的节点
-  //   rclcpp::spin(std::make_shared<PredictorNode>());
-  // 关闭 ROS 2
-  //   rclcpp::shutdown();
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PredictorNode>());
+  rclcpp::shutdown();
   return 0;
 }
